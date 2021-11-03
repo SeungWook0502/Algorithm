6795b4920ff9a8abb62f7b6dcc46d943d067664f
jeonjuho23 <91320503+jeonjuho23@users.noreply.github.com>
2021-11-03 22:53:13 +0900

    BJ_Level01_1

---
+++ JuHo/Level01/Main_1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main_1000 {
+    public static void main(String[] args) {
+        Scanner scan_A = new Scanner(System.in);
+        Scanner scan_B = new Scanner(System.in);
+        int A = scan_A.nextInt();
+        int B = scan_B.nextInt();
+        
+        System.out.println(A+B);
+
+    }
+}
+//예외상황을 없애야함.
