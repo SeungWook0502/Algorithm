ddf1957a0f4cfaa91b54cdb60313e4e1e7cac169
jeonjuho23 <91320503+jeonjuho23@users.noreply.github.com>
2021-11-11 23:10:30 +0900

    Level03-2 (BufferedReader 사용법 익혀야 함)

--- JuHo/Level03/Main_15552.java
+++ JuHo/Level03/Main_15552.java
@@ -1,5 +1,18 @@
+import java.io.*;
 public class Main_15552 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = Integer.parseInt(br.readLine());
+        for(int i=0; i<cnt; i++){
+            int a = Integer.parseInt(br.readLine()), b = Integer.parseInt(br.readLine());
+            bw.write(String.valueOf(a+b));   //BufferedWriter는 String변수만 출력함.
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
+// 왜 런타임 오류지????? 시간 초과로 틀린건가??
+// 샤발;;ㅋㅋㅋ
