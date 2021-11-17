9249cf5ee8aa791b5b0641ff2f5428237c5ad6a9
Hyeon0706 <pooiiiu4100@gmail.com>
2021-11-18 02:33:00 +0900

    Basic Math 2 2/11

--- TaeHyeon/BasicMath2/2581.py
+++ TaeHyeon/BasicMath2/2581.py
@@ -3,10 +3,12 @@ m=int(input())
 n=int(input())
 sosu=[]
 for num in range(m,n+1):
-    arr=[]
-    for i in range(1,num):
-        arr.append(num%i)
-    if arr.count(0)==1:
+    if num==1:
+        continue
+    for j in range(2,num):
+        if num%j==0:
+            break
+    else:
         sosu.append(num)
 if not sosu:
     print(-1)
