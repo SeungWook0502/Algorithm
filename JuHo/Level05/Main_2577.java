8829efee4bea7cbf066bcf2543b6669d974c8c04
jeonjuho23 <91320503+jeonjuho23@users.noreply.github.com>
2021-11-19 02:41:00 +0900

    Level06_1

--- JuHo/Level05/Main_2577.java
+++ JuHo/Level05/Main_2577.java
@@ -7,12 +7,14 @@ public class Main_2577 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int A = Integer.parseInt(br.readLine()), B = Integer.parseInt(br.readLine()), C = Integer.parseInt(br.readLine());
-        String X = String.valueOf(A*B*C);
+        int mul = A*B*C;
+        String X = String.valueOf(mul);
+        char[] X_ch = X.toCharArray();
 
         int[] cnt = new int[10];
         for(int i=0;i<10;i++){
             for(int j=0;j<X.length();j++){
-                if(X.charAt(j)==(char)i)cnt[i]++;
+                if(Character.getNumericValue(X_ch[j]) == i)cnt[i]++;
             }
             System.out.println(cnt[i]);
         }
---
+++ JuHo/Level06/Main_4673.java
@@ -0,0 +1,11 @@
+public class Main_4673 {
+    public static void main(String[] args) {
+        for(int i=0; i<=10000;i++){
+            int n =d(i);
+        }
+    }
+    public static int d (int number){
+
+        return ;
+    }
+}
