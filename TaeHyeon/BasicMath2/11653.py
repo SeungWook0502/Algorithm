b6395ac209aa23509d5cc2e876e454a612583670
Hyeon0706 <pooiiiu4100@gmail.com>
2021-11-18 20:22:55 +0900

    Basic Math 3/11

--- TaeHyeon/BasicMath2/11653.py
+++ TaeHyeon/BasicMath2/11653.py
@@ -1,12 +1,7 @@
-# 왜 시간초과야 외않되 도대체 얼마나 더 줄여달라는거야 ㅁㅇ;ㅣ러매ㅑㅈ덜매쟈더래ㅑㅓㅈㄹ ㅐㅑㅓㅁ
-# 시간초과(211118)
 n=int(input())
 for num in range(2,n+1):
     if n==1:
         break
-    for j in range(2,num):
-        if num%j==0:
-            break
     else:
         while n!=1:
             if n%num!=0:
