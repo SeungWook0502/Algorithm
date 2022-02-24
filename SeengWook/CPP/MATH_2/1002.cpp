09479f5cc646ea0968fe5c195d306557ecdacba4
SeungWook0502 <SeungWook0502@gmail.com>
2022-02-24 15:57:35 +0900

    BAEKJOON STEP MATH_2 - CPP Fin

--- SeengWook/CPP/MATH_2/1002.cpp
+++ SeengWook/CPP/MATH_2/1002.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
-int main(){
+int main() {
+    int x1,y1,r1,x2,y2,r2,d,cond1,cond2,num;
+    cin >> num;
+    for(int i = 0; i < num; i++)
+    {
+        cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+        d = (x1-x2) * (x1-x2) + (y1-y2) * (y1-y2);
+        cond1 = (r1-r2) * (r1-r2);
+        cond2 = (r1+r2) * (r1+r2);
+        if(d == 0)
+        {
+            if(cond1 == 0)
+                cout << "-1" << '\n';
+            else
+                cout << "0" << '\n';
+        }
+        else if (d == cond1 || d == cond2)
+            cout << "1" << '\n';
+        else if (cond1 < d && d < cond2)
+            cout << "2" << '\n';
+        else
+            cout << "0" << '\n';
+    }
 
-	int n;
-	cin >> n;
-	
-	for(int i=0; i<n; i++){
-		int x1, y1, x2, y2, r1, r2;
-		cin >>  x1 >> y1 >> x2 >> y2 >> r1 >> r2;
-		
-		int t1 = sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
-		int t2 = r2+r1;
-		
-		if(t1==0 && r2 == r1) cout << '-1';
-		else if(t1>t2) cout << '0';
-		else if(t1==t2) cout << '1';
-		else if(t1<t2) cout << '2';
-	}
-	return 0;
 }
-
