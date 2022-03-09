f48886c2201df3738e300394da08e67b9118523b
jeonjuho23 <91320503+jeonjuho23@users.noreply.github.com>
2022-03-09 22:11:10 +0900

    math2 5,6,10,11 남음

--- JuHo/1_py/math2_py/1929.py
+++ JuHo/1_py/math2_py/1929.py
@@ -1,20 +1,10 @@
-from re import L
-
-
 m, n = map(int, input().split())
-sosu = []
 
 for num in range(m, n +1):
-    if num == 2:
-        sosu.append(num)
-    elif num > 2:
-        cnt = 0
-        for i in range(2, num):
-            if num % i == 0:
-                cnt += 1
-                break
-        if cnt == 0:
-            sosu.append(num)
-
-for i in sosu:
-    print(i)
+    if num == 1:
+        continue
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            break
+        else:
+            print(num)
