bea90658263f26dc546f853f5f37187a20628e7b
SeungWook0502 <SeungWook0502@gmail.com>
2022-03-14 19:42:33 +0900

    BAEKJOON STEP BRUTEFORCE - CPP

--- SeengWook/CPP/BRUTEFORCE/7568.cpp
+++ SeengWook/CPP/BRUTEFORCE/7568.cpp
@@ -8,23 +8,20 @@ int main(){
 	cin >> n;
 
 	int x[n][2];
-	int c[n];
 
 	for(int i=0; i<n; i++){
 		cin >> x[i][0] >> x[i][1];
-		cout << x[i][0] >> x[i][1];
 	}
 
 	for(int i=0; i<n; i++){
-		c[i]=0;
+		int r = 1;
 		for(int j=0; j<n; j++){
-			if(x[i][0] > x[j][0] && x[i][1] > x[j][1]){
-				c[i]++;
+			if(x[i][0] < x[j][0] && x[i][1] < x[j][1]){
+				r++;
 			}
 		}
+		cout << r << ' ';
 	}
-
-	for(int i=0; i<n; i++) cout << c[i] << '\n';
 
 	return 0;
 }
