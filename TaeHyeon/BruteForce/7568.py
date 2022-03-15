aa84be5144b7741898541f7fa213cba403d6a064
Hyeon0706 <pooiiiu4100@gmail.com>
2022-03-15 17:50:12 +0900

    BruteForce 3/5

--- TaeHyeon/BruteForce/7568.py
+++ TaeHyeon/BruteForce/7568.py
@@ -1,27 +1,13 @@
 def deongchi(member):
-    kgRank=[]
-    cmRank=[]
     for i in member:
-        cnt=1
+        rank=1
         for j in member:
-            if i<j:
-                cnt+=1
-        kgRank.append(cnt)
-    for i in member:
-        cnt=1
-        for j in member:
-            if i[1]<j[1]:
-                cnt+=1
-        cmRank.append(cnt)
-    return kgRank,cmRank
-def rank(kg,cm):
-    pass
+            if i[0]<j[0] and i[1]<j[1]: # 전수 조사를 하며 몸무게와 키가 모두 비교대상보다 작을 때
+                rank+=1 # rank+=1
+        print(rank,end=' ')
 n=int(input())
 member=[]
 for _ in range(n):
     x,y=map(int,input().split())
     member.append([x,y])
-kg,cm=deongchi(member)
-print(kg)
-print(cm)
-rank(kg,cm)
+deongchi(member)
