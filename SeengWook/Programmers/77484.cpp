4b2981155dce9b872f50909569cc727e665d8318
SeungWook0502 <SeungWook0502@gmail.com>
2022-05-24 21:07:41 +0900

    Programmers Level1
    No.77484 로또의 최고 순위와 최저 순위

---
+++ SeengWook/Programmers/77484.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+    vector<int> answer;
+
+    sort(lottos.begin(), lottos.end());
+    sort(win_nums.begin(), win_nums.end());
+
+    int zero = 0;
+    int eqls = 0;
+
+    for(int i=0; i<lottos.size(); i++){
+        for(int j=0; j<lottos.size(); j++){
+            if(lottos[i] == win_nums[j]){
+                eqls++;
+                break;
+            }
+            else if(lottos[i] == 0){
+                zero++;
+                break;
+            }
+            else if(lottos[i] < win_nums[j]) break;
+        }
+    }
+
+    if(eqls+zero<2) answer.push_back(6);
+    else if(eqls+zero>6) answer.push_back(1);
+    else answer.push_back(7-(eqls+zero));
+
+    if(eqls<2) answer.push_back(6);
+    else answer.push_back(7-eqls);
+
+    for(int i=0; i<answer.size(); i++) cout << answer[i] << ' ';
+
+    return answer;
+}
+
+int main(){
+
+    vector<int> lottos = {45, 4, 35, 20, 3, 9};
+    vector<int> win_nums = {20, 9, 3, 45, 4, 35};
+
+    lottos = solution(lottos, win_nums);
+
+    return 0;
+}
